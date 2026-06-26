7707ab90a883f32d0c3dc464cf47a0485b217b13
Siddharth Subramaniam <siddharthsubramaniam1996@gmail.com>
2026-06-26 17:49:18 +0530

    chore: use akshaya.local hostname in start.bat

--- start.bat
+++ start.bat
@@ -4,7 +4,7 @@ echo Starting Karaoke App...
 docker run --pull always --name karaoke --rm -p 7860:7860 siddharths96/karaoke
 echo.
 echo On this computer : http://localhost:7860
-echo On your iPhone   : http://karaoke.local:7860
+echo On your iPhone   : http://akshaya.local:7860
 echo (iPhone must be on the same WiFi)
 echo.
 echo Close this window to stop the app.
