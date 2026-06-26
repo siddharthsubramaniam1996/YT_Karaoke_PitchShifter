7e25885b2893858e7751ce8c4709a57f7b48c332
Siddharth Subramaniam <siddharthsubramaniam1996@gmail.com>
2026-06-26 17:41:34 +0530

    chore: update start.bat to pull from siddharths96/karaoke on Docker Hub

--- start.bat
+++ start.bat
@@ -1,14 +1,10 @@
 @echo off
 docker rm -f karaoke 2>nul 1>nul
-echo Building Karaoke App (first time takes a few minutes)...
-docker build -t karaoke . || (echo. & echo Something went wrong. Send a screenshot to your brother. & pause & exit /b 1)
-echo.
-echo App is ready^^!
+echo Starting Karaoke App...
+docker run --pull always --name karaoke --rm -p 7860:7860 siddharths96/karaoke
 echo.
 echo On this computer : http://localhost:7860
 echo On your iPhone   : http://karaoke.local:7860
 echo (iPhone must be on the same WiFi)
 echo.
 echo Close this window to stop the app.
-echo.
-docker run --name karaoke --rm -p 7860:7860 karaoke
